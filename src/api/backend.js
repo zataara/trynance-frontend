38ec053377b0567e2ee39d13b6334f15e42cb628
zataara <bryantmac@gmail.com>
2022-01-04 17:46:13 -0800

    added api helper for talking to backend

---
+++ src/api/backend.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+
+const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3003";
+
+class BackendApi {
+  static token;
+
+  static async request(endpoint, data = {}, method = "get") {
+    console.debug("API Call:", endpoint, data, method);
+
+    const url = `${BASE_URL}/${endpoint}`;
+    const headers = { Authorization: `Bearer ${BackendApi.token}` };
+    const params = method === "get" ? data : {};
+
+    try {
+      return (await axios({ url, method, data, params, headers })).data;
+    } catch (e) {
+      console.error("API Error:", e.response);
+      let message = e.response.data.error.message;
+      throw Array.isArray(message) ? message : [message];
+    }
+  }
+
+  
+  static async login(data) {
+    let res = await this.request(`auth/token`, data, 'post');
+    return res.token;
+  }
+
+  static async register(data) {
+    let res = await this.request(`auth/register`, data, "post");
+    return res.token;
+  }
+}
+
+module.exports = BackendApi;
