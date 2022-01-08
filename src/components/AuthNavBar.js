2c53c2fffc268787802f8df738db102ffd73b87a
zataara <bryantmac@gmail.com>
2022-01-07 20:24:38 -0800

    setup basic auth only navigation bar

---
+++ src/components/AuthNavBar.js
@@ -0,0 +1,67 @@
+import { React, useState } from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const AuthNavBar = (props) => {
+  const [navOpen, setNavOpen] = useState(false);
+  return (
+    <>
+      <nav className="top-0 absolute z-50 w-full felx flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg">
+        <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+          <div ClassName="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+            <Link
+              className="text-black text-sm font-bold leading-relaxed inine-block mr-4 py-2 whitespace-nowrap uppercase"
+              to="/"
+            >
+              Trynance
+            </Link>
+            <button
+              className="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+              type="button"
+              onClick={() => setNavOpen(!navOpen)}
+            >
+              <i className="text-white fas fa-bars"></i>
+            </button>
+          </div>
+          <div
+            className={
+              "lg:flex flex-grow items-center bg-white lg:bg-opacity-0 lg:shadow-none" +
+              (navOpen ? " block rounded shadow-lg" : " hidden")
+            }
+            id="example-navbar-warning"
+          >
+            <ul className="flex flex-col lg:flex-row list-none mr-auto">
+              <li className="flex items-center"></li>
+            </ul>
+            <ul classname="flex flex-col lg:flex-row list-none lg:ml-auto">
+              <li className="flex items-center">
+                <Link to='/register'>
+                <button
+                  className="bg-white text-blueGray-700 active:bg-blueGray-50 text-xs font-bold uppercase px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3 ease-linear transition-all duration-150"
+                  type="button"
+                >
+                  Register
+                </button>
+                </Link>
+              </li>
+            </ul>
+            <ul>
+              <li className="flex items-center">
+                <Link to='/login'>
+                <button
+                  className="bg-white text-blueGray-700 active:bg-blueGray-50 text-xs font-bold uppercase px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3 ease-linear transition-all duration-150"
+                  type="button"
+                >
+                  Login
+                </button>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default AuthNavBar;
