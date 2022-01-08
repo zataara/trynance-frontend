d1826eb2ddfaa7394145ce4f204db325ef165bae
zataara <bryantmac@gmail.com>
2022-01-07 23:49:46 -0800

    initial page creation

---
+++ src/pages/Trades.js
@@ -0,0 +1,12 @@
+
+
+const Trades = () => {
+  return (
+    <div className='container'>
+      <h1>Page Not Found!</h1>
+    </div>
+  )
+  
+  }
+  
+  export default Trades;
---
+++ src/pages/User.js
@@ -0,0 +1,13 @@
+
+
+
+const User = () => {
+  return (
+    <div className='container'>
+      <h1>Page Not Found!</h1>
+    </div>
+  )
+  
+  }
+  
+  export default User;
