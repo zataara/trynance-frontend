021eead99788446d52ee71d404c8db1517e30740
zataara <bryantmac@gmail.com>
2022-01-08 23:47:11 -0800

    changed js object style of useRoutes to new version

--- src/routes.js
+++ src/routes.js
@@ -1,50 +1,102 @@
-import { useRoutes, Navigate } from 'react-router-dom';
+import { Routes, Route, useRoutes, Navigate } from "react-router-dom";
 
 /*****  Layouts  *****/
-import TradeLayout from './layouts/TradeLayout';
-import UnAuthLayout from './layouts/UnAuthLayout';
+import PrivateRoute from "./layouts/PrivateRoute";
 
 /***** Auth *****/
-import Login from './auth/Login';
-import Register from './auth/Register';
+import Login from "./auth/Login";
+import Register from "./auth/Register";
 
 /***** Pages *****/
-import Trade from './pages/Trade';
-import User from './pages/User';
-import Trades from './pages/Trades';
-import NotFound from './pages/NotFound';
+import Dashboard from "./pages/Dashboard";
+import Assets from "./pages/Assets";
+import Trades from "./pages/Trades";
+import Gainers from "./pages/Gainers";
+import Losers from "./pages/Losers";
+import Profile from "./pages/Profile";
+import NotFound from "./pages/NotFound";
 
-
-const Router = () => {
-  return useRoutes([
-    {
-      path: '/home',
-      element: <TradeLayout />,
-      children: [
-        { element: <Navigate to='/home/trade' replace />},
-        { path: 'trade', element: <Trade /> },
-        { path: 'user', element: <User />},
-        { path: 'trades', element: <Trades />},
-
-      ]
-    }, 
-    {
-      path: '/',
-      element: <UnAuthLayout />,
-      children: [
-        { path: 'login', element: <Login />},
-        { path: 'register', element: <Register />},
-        { path: '404', element: <NotFound />},
-        { path: '/', element: <Navigate to='/home' />},
-        {path: '*', element: <Navigate to='/404' />}
-      ]
-    },
-    { path: '*', element: <Navigate to="/404" replace />}
-  ])
-
-
-}
-
-
+const Router = ({ login, register, logout }) => {
+  console.debug(
+    "Routes",
+    `login=${typeof login}`,
+    `register=${typeof register}`
+  );
+  return (
+    <>
+      <Routes>
+        <Route exact path="/login" element={<Login login={login} />} />
+        <Route
+          exact
+          path="/register"
+          element={<Register register={register} />}
+        />
+        <Route
+          exact
+          path="/app"
+          element={
+            <PrivateRoute exact path="/app">
+              <Dashboard />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          exact
+          path="/assets"
+          element={
+            <PrivateRoute exact path="/assets">
+              <Assets />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          exact
+          path="/trades"
+          element={
+            <PrivateRoute exact path="/trades">
+              <Trades />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          exact
+          path="/gainers"
+          element={
+            <PrivateRoute exact path="/gainers">
+              <Gainers />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          exact
+          path="/losers"
+          element={
+            <PrivateRoute exact path="/losers">
+              <Losers />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          exact
+          path="/profile"
+          element={
+            <PrivateRoute exact path="/profile">
+              <Profile />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/404"
+          element={
+            <PrivateRoute exact path="/404">
+              <NotFound />
+            </PrivateRoute>
+          }
+        />
+        {/* <Navigate path="*" to="/" /> */}
+      </Routes>
+    </>
+  );
+};
 
 export default Router;
