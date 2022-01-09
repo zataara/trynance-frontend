313e32ee651f9531fed695baf5bba848e5100dda
zataara <bryantmac@gmail.com>
2022-01-08 23:29:30 -0800

    adjusted sytax

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,17 +1,21 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react"
 
-const useLocalStorage = (key, initialValue = []) => {
-  if (localStorage.getItem(key)) {
-    initialValue = JSON.parse(localStorage.getItem(key));
-  }
-  const [value, setValue] = useState(initialValue);
-  
-  const clearLocalStorage = () => setValue("");
+function useLocalStorage(key, firstValue = null) {
+  const initialValue = localStorage.getItem(key) || firstValue;
 
-  useEffect(() => {
-    localStorage.setItem(key, JSON.stringify(value));
-  }, [value, key]);
-  return [value, setValue, clearLocalStorage];
-};
+  const [item, setItem] = useState(initialValue);
+
+  useEffect(function setKeyInLocalStorage() {
+    console.debug("hooks useLocalStorage useEffect", "item=", item);
+
+    if (item === null) {
+      localStorage.removeItem(key);
+    } else {
+      localStorage.setItem(key, item);
+    }
+  }, [key, item]);
+
+  return [item, setItem];
+}
 
 export default useLocalStorage;
