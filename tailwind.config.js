48bfd87fbebf54f0fef7cd8e74d397e99c4cb5cf
zataara <bryantmac@gmail.com>
2022-01-09 20:39:06 -0800

    adding custom colors for table backgrounds

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "darkmode-tbody": "#1E2838",
+        "darkmode-thead": "#10192B",
+      }
+    },
   },
   plugins: [],
 };
+
+
