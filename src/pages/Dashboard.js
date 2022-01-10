8e37fd7aeaf46210bdf5599f844d9cf889862e29
zataara <bryantmac@gmail.com>
2022-01-09 22:42:45 -0800

    added dashboard view

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Cryptos from "../components/Cryptos";
+
+const Dashboard = () => {
+  return (
+    <>  
+      <Cryptos />
+    </>
+  )
+  
+};
+
+export default Dashboard;
