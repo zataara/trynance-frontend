5170435a15ae36b8cadb9ba73ae1ce01efe2b811
zataara <bryantmac@gmail.com>
2022-01-10 23:45:11 -0800

    changed folder structure

--- src/layouts/PrivateRoute.js
+++ src/layouts/PrivateRoute.js
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import { Navigate } from "react-router-dom";
-import UserContext from "../auth/UserContext";
+import UserContext from "../context/UserContext";
 import TradeLayout from "./TradeLayout";
 
 /** "Higher-Order Component" for private routes.
