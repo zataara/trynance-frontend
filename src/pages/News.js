41dbd7f7610e0a05ca92d63cec199e74caf1fa84
zataara <bryantmac@gmail.com>
2022-01-17 21:32:05 -0800

    adding div for news holder

--- src/pages/News.js
+++ src/pages/News.js
@@ -1,10 +1,27 @@
+import {React, useContext} from "react";
+import UserContext from "../context/UserContext";
+
 
 const News = () => {
-
+  const {
+    currentUser,
+    faves,
+    trades,
+    assets,
+    news,
+  } = useContext(UserContext);
   return (
     <>
+      <div className="flex flex-col md:mx-20 h-screen overscroll-none mr-5">
+        <div className="overflow-x-auto sm:-mx-6 lg:-mx-8 overscroll-none">
+          <div className="inline-block pt-14 py-2 min-w-full sm:px-6 lg:px-8 overscroll-none">
+          
+
+          </div>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
 export default News;
