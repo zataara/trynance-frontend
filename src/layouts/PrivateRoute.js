2a31a9aff362d765f6ff02b9fa797e2f3d43859d
zataara <bryantmac@gmail.com>
2022-01-19 15:51:46 -0800

    drilling logout prop

--- src/layouts/PrivateRoute.js
+++ src/layouts/PrivateRoute.js
@@ -10,10 +10,10 @@ import TradeLayout from "./TradeLayout";
  * route if so. If no user is present, redirects to login form.
  */
 
-function PrivateRoute({ children }) {
+function PrivateRoute({ children, logout }) {
   const { currentUser } = useContext(UserContext);
 
-  return currentUser ? <TradeLayout>  {children} </TradeLayout> : <Navigate to="/login" />
+  return currentUser ? <TradeLayout logout={logout}>  {children} </TradeLayout> : <Navigate to="/login" />
 }
 
 export default PrivateRoute;
