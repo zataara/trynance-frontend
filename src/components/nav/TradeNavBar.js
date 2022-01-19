c1b2fa1a605a6af9ee18aea2980d87d7047a872e
zataara <bryantmac@gmail.com>
2022-01-19 15:51:24 -0800

    changed link paths

--- src/components/nav/TradeNavBar.js
+++ src/components/nav/TradeNavBar.js
@@ -31,7 +31,7 @@ const TradeNavBar = (props) => {
 
             <ul>
               <li className="flex items-center">
-                <Link to="/login">
+                <Link to="/app">
                   <button
                     className="bg-white text-blueGray-700 active:bg-blueGray-50 text-xs font-bold uppercase px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3 ease-linear transition-all duration-150"
                     type="button"
