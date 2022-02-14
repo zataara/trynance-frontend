014eced308f26e50c64b66fd5a54ccc5a090048a
zataara <bryantmac@gmail.com>
2022-02-14 13:29:01 -0800

    adding responsive attributes to componenet

--- src/components/ComingSoon.js
+++ src/components/ComingSoon.js
@@ -1,12 +1,11 @@
 import React from "react";
 
 const ComingSoon = () => {
-
   return (
     <>
-      <div className="container mx-auto px-4 h-full lg:mr-20">
-        <div className="flex content-center items-center justify-center h-full">
-          <div className="w-full md:w-6/12 px-4 md:mr-72 md:mb-48">
+      <div className="flex flex-col md:items-center md:ml-64 my-10 h-screen">
+        <div className="overflow-x-auto md:w-6/12 sm:-mx-6 overscroll-none">
+          <div className="inline-block pt-14 py-2 min-w-full sm:px-6">
             <div className="relative flex flex-col min-w-0 break-words w-full mb-6 mt-28 shadow-lg rounded-lg bg-gradient-to-r from-cyan-500 to-blue-500 border-0">
               <div className="rounded-t mb-0 px-6 py-6">
                 <div className="text-center mb-3">
